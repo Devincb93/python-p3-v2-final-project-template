be0cd53128e61e2602e5335d168adfff8a530041
Devin <devincb93@gmail.com>
2024-06-23 17:57:40 -0700

    updated

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,4 @@
 # lib/cli.py
-
 from models.Book import Book
 from rich.console import Console
 from rich.table import Table
@@ -13,12 +12,17 @@ from helpers import (
     menu,
     list_all_books,
     get_all_authors,
-    find_books_by_title
+    find_books_by_title,
+    delete_book,
+    add_a_author,
+    delete_author,
+    select_an_author,
+    authors_menu,
+    book_menu_text,
+    library_menu_text,
+    find_books_by_author
 )
-
-
 console = Console()
-
 invalid = pyfiglet.figlet_format("Invalid", font="mini",)
 
 
@@ -33,35 +37,19 @@ def main():
         else:
             console.print("[bold red]*****Invalid choice*****[/bold red]")
 
-def delete_book():
-    books = Book.get_all()
-    table = Table(title="Available Books", border_style="blue")
-    table.add_column("Book Titles")
-    for book in books:
-        table.add_row(book.title)
-    console.print(table)
-    console.print(Panel("Enter the name of the book you want to Delete:", border_style="blue"))
-    delete_selection = input("Title: ")
-    if len(delete_selection) > 0:
-        for book in books:
-            if delete_selection in book.title:
-                book.delete()
-                console.print(f"Book:{book.title} deleted successfuly ")
-    else:
-        library_menu()
-
 def library_menu():
     while True:
-        console.print(Panel("Library Menu:", border_style="blue"))
-        console.print("1) List all authors")
-        console.print("2) Add a author")
-        console.print("3) Delete a author")
-        console.print("4) List all books")
-        console.print("5) Add a book by author")
-        console.print("6) Delete a book")
-        console.print("7) Find books by title")
-        console.print("8) Find book by author")
-        console.print("0) Exit to Main Menu")
+        authors_menu()
+        choice = input("> ")
+        if choice == "1":
+            library_menus()
+        if choice == "2":
+            book_menu()
+
+
+def library_menus():
+    while True:
+        library_menu_text()
         
         choice = input("> ")
         if choice == "1":
@@ -73,98 +61,36 @@ def library_menu():
         if choice == "4":
             list_all_books()
         if choice == "5":
-            add_book_by_author()
+            select_an_author()
+
+
         if choice == "6":
             delete_book()
+            
         if choice == "7":
             find_books_by_title()
         if choice == "8":
             find_books_by_author()
         elif choice == "0":
-            main()
+            library_menu()
         else:
             console.print("")
 
-def add_a_author():
-    console.print("Enter the name of the author to add:")
-    name = input("Name: ")
-    if len(name) > 0:
-        if not Author.find_by_name(name):
-            Author.create(name)
-            console.print(f"{name} has been added successfully")
-        else:
-            console.print(f"[bold red]{name}, already exists. Please enter a new Author.[/bold red]")
-            add_a_author()
-    else:
-        library_menu()
-
-def delete_author():
-    authors = Author.get_all()
-    author_table = Table(title="Authors")
-    author_table.add_column("Names:")
-    if len(authors) > 0:
-        for author in authors:
-            author_table.add_row(author.name)
-        console.print(author_table)
-    else:
-        console.print("No authors exist, please add one")
-        add_a_author()
-    console.print("Enter the name of the author you would like to remove:")
-    name_to_delete = input("Name: ")
-    for author in authors:
-        if name_to_delete == author.name:
-            author.delete()
-            console.print(f"{author.name} was deleted successfully")
-        
-
-def add_book_by_author():
+def book_menu():
     while True:
-        selection_number = 1
-        authors = Author.get_all()
-        console.print(Panel("Select a Author, or press Enter to go back:"))
-        for author in authors:
-            console.print(f" {author.name}")
-            selection_number += 1
-
-        
-        
+        book_menu_text()
         choice = input("> ")
-        if choice == "":
-            menu()
-        for author in authors:
-
-            if choice == (f"{author.name}"):
-                    add_authors_book(author.id)
+        if choice == "1":
+            find_books_by_title()
+            book_menu()
+        if choice == "2":
+            find_books_by_author()
         else:
-            break
-        
-        
-def add_authors_book(id):
-    author = Author.find_by_id(id)
-    console.print("Enter the book information:")
-    title = input("Title: ")
-    genre = input("Genre: ")
-    author_id = author.id 
-    Book.create(title, genre, author_id)
-    menu()   
+            library_menu()
 
 
-def find_books_by_author():
-    authors = Author.get_all()
-    books = Book.get_all()
-    book_author_table = Table(title="Found books:", border_style="blue")
-    book_author_table.add_column("Titles")
-    console.print(Panel("Enter the name of the author:"))
-    authors_name = input("Name: ")
-    if len(authors_name) > 0:
-        for author in authors:
-            if authors_name in author.name:
-                for book in books:
-                    if book.author_id == author.id:
-                        book_author_table.add_row(book.title)
-    else:
-        library_menu()
-    console.print(book_author_table)
+
+
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -21,15 +21,98 @@ invalid = pyfiglet.figlet_format("Invalid", font="small",)
 def menu():
     console.print(Panel(ascii_art, border_style="blue"))
     console.print(Panel("[bold]Please select an option:[/bold]", border_style="blue"))
-    console.print("1) Library Terminal")
+    console.print("1) Start Program")
     console.print("0) Exit Program")
+
+def authors_menu():
+    console.print(Panel("Library Menu:", border_style="blue"))
+    console.print("1) Author's Menu")
+    console.print("2) Book's Menu")
+
+def book_menu_text():
+    console.print(Panel("Book's Menu: ", border_style="blue"))
+    console.print("1) Search for book by title")
+    console.print("2) Search for books by Author")
+
+
+def library_menu_text():
+    console.print(Panel("Author's Menu:", border_style="blue"))
+    console.print("1) List all authors")
+    console.print("2) Add an author")
+    console.print("3) Delete a author")
+    console.print("4) List all books")
+    console.print("5) Pick an author and add a book")
+    console.print("0) Exit to Main Menu")
 
 
 def exit_program():
     console.print(Panel("[bold red]Program Terminated[/bold red]", border_style="blue"))
     exit()
 
+def add_a_author():
+    console.print("Enter the name of the author to add:")
+    name = input("Name: ")
+    if len(name) > 0:
+        if not Author.find_by_name(name):
+            Author.create(name)
+            console.print(f"{name} has been added successfully")
+        else:
+            console.print(f"[bold red]{name}, already exists. Please enter a new Author.[/bold red]")
+            add_a_author()
 
+def delete_author():
+    authors = Author.get_all()
+    author_table = Table(title="Authors")
+    author_table.add_column("Names:")
+    if len(authors) > 0:
+        for author in authors:
+            author_table.add_row(author.name)
+        console.print(author_table)
+    else:
+        console.print("No authors exist, please add one")
+        add_a_author()
+    console.print("Enter the name of the author you would like to remove:")
+    name_to_delete = input("Name: ")
+    for author in authors:
+        if name_to_delete == author.name:
+            author.delete()
+            console.print(f"{author.name} was deleted successfully")
+
+def select_an_author():
+        authors = Author.get_all()
+        for i, author in enumerate(authors, start=1):
+            console.print(f"{i}) {author.name}")
+        choice = input("> ")
+        if 0 <= int(choice) <= len(authors):
+            selected_author = authors[int(choice) -1]
+            add_authors_book(selected_author)
+    
+
+def add_authors_book(author):
+    # authors = Author.get_all()
+    # selected_author = authors[i]
+    console.print("Enter the book information:")
+    title = input("Title: ")
+    genre = input("Genre: ")
+    author_id = author.id
+    Book.create(title, genre, author_id)
+    menu()
+    
+
+def delete_book():
+    books = Book.get_all()
+    table = Table(title="Available Books", border_style="blue")
+    table.add_column("Book Titles")
+    for book in books:
+        table.add_row(book.title)
+    console.print(table)
+    console.print(Panel("Enter the name of the book you want to Delete:", border_style="blue"))
+    delete_selection = input("Title: ")
+    if len(delete_selection) > 0:
+        for book in books:
+            if delete_selection in book.title:
+                book.delete()
+                console.print(f"Book:{book.title} deleted successfuly ")
 
 
 def list_all_books():
@@ -52,18 +135,32 @@ def get_all_authors():
         console.print("[bold red]*****No authors found*****[/bold red]")
 
 def find_books_by_title():
-
-    console.print(Panel("Enter the title of the book you are searching for", border_style="blue"))
-    search = input(str("Title: "))
-    if len(Book.get_all()) > 0 and len(search) > 0:
-        for book in Book.get_all():
-            if search in book.title:
+    
+        console.print(Panel("Enter the title of the book you are searching for", border_style="blue"))
+        search = input(str("Title: "))
+        if len(Book.get_all()) > 0 and len(search) > 0:
+            searches = []
+            for book in Book.get_all():
                 searched = Table(border_style="blue")
                 searched.add_column("Your Search:")
-                searched.add_row(book.title)
-                console.print(searched)
-    else:
-        menu()
+                if search.lower() in book.title.lower():
+                    searches.append(book.title)
+                for sear in searches:   
+                    searched.add_row(sear)
+        console.print(searched)
+
+def find_books_by_author():
+    found_books = Author.books()
+    console.print(found_books)
+            
+        
+        
+        
+    
+    
+   
+    
+        
     
 
         
